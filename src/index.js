4ce0a67d62c0d3c863829d7040a22a4e50c24774
范文博 <fanwenbo@fanwenbodeMacBook-Pro.local>
2019-07-25 18:21:09 +0800

    feat: 构建webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+console.log(App, 'app')
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve('./dist')
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    devServer: {
+        historyApiFallback: true,
+        inline: true,
+        progress: true,
+        overlay: true,
+        hot: true,
+        host: '127.0.0.1',
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: /src/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react',
+                    ],
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            inject: true,
+            hash: true
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
